254c192f1c541f5fc1aa938cbe81ce470dd1ab6b
Lauri <tikkalauri1@gmail.com>
2025-03-12 15:30:45 +0200

    Added templates

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Tervetuloa kirjainlaatikkoon, tämä on elokuvien arvostelupalvelu"
+    return render_template("index.html")
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def page2():
+    return "Tämä on sivu 2"
+
+@app.route("/numbers")
+def numbers():
+    content = ""
+    for i in range(1,101):
+        content += str(i) + " "
+    return content
+
+if __name__ == "__main__":
+    app.run(debug=False)
