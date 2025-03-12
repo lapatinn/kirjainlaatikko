be81615fa2924900b68af5f450341a0dd17b89c7
Lauri <tikkalauri1@gmail.com>
2025-03-12 17:04:39 +0200

    modified app.py to include form functions, modified index.html to look better

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    movies = ["Terminator", "Star Wars", "Madagascar",]
+    return render_template("index.html", message="Parhaat elokuvat 2025", items=movies)
 
 @app.route("/page1")
 def page1():
-    return "Tämä on sivu 1"
+    return "Tämä on sivu 1, tänne tulee varmaan jotain joskus"
 
 @app.route("/page2")
 def page2():
-    return "Tämä on sivu 2"
+    return "Tämä on sivu 2, tännekin saatta joskus tulla jotain"
 
 @app.route("/numbers")
 def numbers():
@@ -21,6 +22,15 @@ def numbers():
     for i in range(1,101):
         content += str(i) + " "
     return content
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    message = request.form["message"]
+    return render_template("result.html", message=message)
 
 if __name__ == "__main__":
     app.run(debug=False)
